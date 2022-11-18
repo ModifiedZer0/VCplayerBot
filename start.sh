fb02cb7d28e3a6195ceacf39cb5c8b9a3331dae6
Baibhav Saini <90299581+ModifiedZer0@users.noreply.github.com>
2022-11-18 13:25:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [[ $BRANCH != "None" ]]
 then
   echo "Cloning beta branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/ModifiedZer0/VCPlayerBot -b $BRANCH /VCPlayerBot
 else
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/ModifiedZer0/VCPlayerBot /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
